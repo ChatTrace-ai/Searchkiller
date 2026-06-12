5f388f7b58f96873b5389b7157bd8a7cdbda71be
Zhighway777 <1990405801@qq.com>
2026-06-13 00:05:24 +0800

    Revert "feat: support ES_URL for Elasticsearch Serverless connections"
    This reverts commit 52b1947677d8258be2509b04caf78d19aaac9f4a.

--- lib/es-client.ts
+++ lib/es-client.ts
@@ -4,27 +4,13 @@ let _client: Client | null = null;
 
 export function getClient(): Client {
   if (!_client) {
-    const apiKey = process.env.ES_API_KEY;
-    if (!apiKey) {
-      throw new Error('ES_API_KEY must be set');
+    if (!process.env.ES_CLOUD_ID || !process.env.ES_API_KEY) {
+      throw new Error('ES_CLOUD_ID and ES_API_KEY must be set');
     }
-
-    const esUrl = process.env.ES_URL;
-    const cloudId = process.env.ES_CLOUD_ID;
-
-    if (esUrl) {
-      _client = new Client({
-        node: esUrl,
-        auth: { apiKey },
-      });
-    } else if (cloudId) {
-      _client = new Client({
-        cloud: { id: cloudId },
-        auth: { apiKey },
-      });
-    } else {
-      throw new Error('Either ES_URL or ES_CLOUD_ID must be set');
-    }
+    _client = new Client({
+      cloud: { id: process.env.ES_CLOUD_ID },
+      auth: { apiKey: process.env.ES_API_KEY },
+    });
   }
   return _client;
 }
